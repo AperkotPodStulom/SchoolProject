1e23498d64bdf360aec2798bf958f13e7f215cd9
AperkotPodStulom <97829944+AperkotPodStulom@users.noreply.github.com>
2022-10-29 21:12:54 +0700

    First Commit

---
+++ Assets/Wizard - 2D Character/Demo/SimplePlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace ClearSky
+{
+    public class SimplePlayerController : MonoBehaviour
+    {
+        public float movePower = 10f;
+       
+        private Animator anim;
+        private int direction = 1;
+        
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            //running
+            Vector3 moveVelocity = Vector3.zero;
+            anim.SetBool("isRun", false);
+
+
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                direction = -1;
+                moveVelocity = Vector3.left;
+
+                transform.localScale = new Vector3(direction, 1, 1);
+                if (!anim.GetBool("isJump"))
+                    anim.SetBool("isRun", true);
+
+            }
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                direction = 1;
+                moveVelocity = Vector3.right;
+
+                transform.localScale = new Vector3(direction, 1, 1);
+                if (!anim.GetBool("isJump"))
+                    anim.SetBool("isRun", true);
+
+            }
+            transform.position += moveVelocity * movePower * Time.deltaTime;
+        }
+    }
+}
