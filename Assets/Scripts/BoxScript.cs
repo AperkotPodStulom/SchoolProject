c0bcf4e2424552bc779651906b00403e9484ef38
AperkotPodStulom <97829944+AperkotPodStulom@users.noreply.github.com>
2022-10-31 21:51:39 +0700

    I played with physics

--- Assets/Scripts/BoxScript.cs
+++ Assets/Scripts/BoxScript.cs
@@ -5,5 +5,32 @@ using GlobalThings;
 
 public class BoxScript : MonoBehaviour
 {
-    
+    [SerializeField] private int boxHealth;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(boxHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        boxHealth -= 1;
+
+        if (collision.gameObject.tag == "Player" && rb.velocity.magnitude >= 10)
+            boxHealth -= boxHealth;
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(boxHealth);
+    }
 }
